882bfca208d998f8e5b28affed7b479da9d11f49
Nicolas Herrera <herreran903@gmail.com>
2024-08-18 16:26:44 -0500

    Initial Commit.

---
+++ src/test/java/com/api_stock/stock/ApiStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_stock.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
